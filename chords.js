cc765a46748634850376a006d0963cd70731e4da
kanatch_7777 <kanatch271828@gmail.com>
2026-06-21 13:43:38 +0900

    fix

--- chords.js
+++ chords.js
@@ -12,6 +12,7 @@ const keys = [
   466.16,
   493.88
 ];
+let chordProg = []
 
 //コードの中身は設定で変える。(ダイアトニックだけなのか、モードを変えたりするのかとか)
 const chords = {
@@ -28,7 +29,6 @@ const start = document.getElementById("start")
 start.addEventListener('click',()=>{
     let rmIdx = Math.floor(Math.random() * keys.length);
     let key=keys[rmIdx]
-    let chordProg = []
     for (let i = 0;i<4;i++) {
         let rmIdx = Math.floor(Math.random() * chords.length);
         chordProg.push(chords[rmIdx])
