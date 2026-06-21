9aeab3c1a1b55e56ca2feda0ef762eb36c51e22a
kanatch_7777 <kanatch271828@gmail.com>
2026-06-21 13:35:49 +0900

    fix

--- chords.js
+++ chords.js
@@ -15,16 +15,16 @@ const keys = [
 
 //コードの中身は設定で変える。(ダイアトニックだけなのか、モードを変えたりするのかとか)
 const chords = {
-    "1":{deg:1, type:maj}
-    ,"2m":{deg:2, type:min}
-    ,"3":{deg:3, type:maj}
-    ,"4":{deg:4, type:maj}
-    ,"5":{deg:5, type:maj}
-    ,"6":{deg:6, type:min}
-    ,"7":{deg:7, type:dim}
+    "1":{deg:1, type:"maj"}
+    ,"2m":{deg:2, type:"min"}
+    ,"3":{deg:3, type:"maj"}
+    ,"4":{deg:4, type:"maj"}
+    ,"5":{deg:5, type:"maj"}
+    ,"6":{deg:6, type:"min"}
+    ,"7":{deg:7, type:"dim"}
 }
 
-const start = document.getElementById('start')
+const start = document.getElementById("start")
 start.addEventListener('click',()=>{
     let rmIdx = Math.floor(Math.random() * keys.length);
     let key=keys[rmIdx]
